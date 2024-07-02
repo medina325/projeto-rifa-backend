411123d8a0b7c004c62d9a4c0a0afe2072d1035c
medina325 <medina_cdz@hotmail.com>
2024-07-02 14:11:05 -0400

    Criando tabelas do modelo ER

--- alembic/env.py
+++ alembic/env.py
@@ -18,7 +18,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from app.database import Base
+from app.models import Base
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
@@ -63,10 +63,11 @@ def run_migrations_online() -> None:
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
-
+    
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata
         )
 
         with context.begin_transaction():
--- app/models.py
+++ app/models.py
@@ -1,16 +1,21 @@
-import uuid
 from sqlalchemy import (
     Column,
     String,
     Integer,
     TIMESTAMP,
+    Date,
+    Float,
     DateTime,
-    ForeignKey,
+    Boolean,
     Text,
+    ForeignKey,
     func
 )
-from sqlalchemy.dialects.sqlite import BLOB
-from app.database import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = "users"
@@ -32,3 +37,57 @@ class RevokedToken(Base):
 
     token = Column(String, primary_key=True, index=True)
     revoked_at = Column(DateTime(timezone=True), server_default=func.now())
+
+class Rifa(Base):
+    __tablename__ = 'rifas'
+    
+    rifa_id = Column(Integer, primary_key=True)
+    criador_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    nome = Column(String(30), nullable=False)
+    descricao = Column(Text)
+    status = Column(Integer, nullable=False)
+    preco_bilhete = Column(Float, nullable=False)
+    premio_nome = Column(String(50), nullable=False)
+    premio_imagem = Column(String, nullable=False)
+    data_sorteio = Column(Date, nullable=False)
+    
+    criador = relationship("User")
+    created_at = Column(TIMESTAMP, server_default=func.current_timestamp())
+    updated_at = Column(TIMESTAMP, server_default=func.current_timestamp(), onupdate=func.current_timestamp())
+
+class Comprador(Base):
+    __tablename__ = 'compradores'
+    
+    comprador_id = Column(Integer, primary_key=True)
+    nome = Column(String(80), nullable=False)
+    numero_celular = Column(String(11), nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
+
+    created_at = Column(TIMESTAMP, server_default=func.current_timestamp())
+    updated_at = Column(TIMESTAMP, server_default=func.current_timestamp(), onupdate=func.current_timestamp())
+
+class Bilhete(Base):
+    __tablename__ = 'bilhetes'
+    
+    bilhete_id = Column(Integer, primary_key=True)
+    rifa_id = Column(Integer, ForeignKey('rifas.rifa_id'), nullable=False)
+    comprador_id = Column(Integer, ForeignKey('compradores.comprador_id'))
+    vendido = Column(Boolean, nullable=False, default=False)
+    
+    rifa = relationship("Rifa")
+    comprador = relationship("Comprador")
+    
+    created_at = Column(TIMESTAMP, server_default=func.current_timestamp())
+    updated_at = Column(TIMESTAMP, server_default=func.current_timestamp(), onupdate=func.current_timestamp())
+
+class Pagamento(Base):
+    __tablename__ = 'pagamentos'
+    
+    pagamento_id = Column(Integer, primary_key=True)
+    bilhete_id = Column(Integer, ForeignKey('bilhetes.bilhete_id'), unique=True, nullable=False)
+    valor = Column(Float, nullable=False)
+
+    # NOTE Adicionar outras colunas necessárias após descobrir como adicionar pagamento
+
+    created_at = Column(TIMESTAMP, server_default=func.current_timestamp())
+    updated_at = Column(TIMESTAMP, server_default=func.current_timestamp(), onupdate=func.current_timestamp())
